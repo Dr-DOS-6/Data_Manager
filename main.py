9cdf8db0cf17e3b994c3f80546c90cba6fe743cc
Dr-DOS-6 <intelfan6125@gmail.com>
2023-04-12 08:47:11 +0900

    表記を改善

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ count =0
 var_list = []
 appendcount = int(input("How many datas will you enter? "))
 while stat == 0:
-  exectxt = f'{str(count+1)}つ目のデータを入力してください。'
+  exectxt = f'{str(count+1)}個目のデータを入力してください。'
   if len(var_list) == appendcount:
     stat = int(input("データ入力を終了しますか？ 0:続ける 1:終了 "))
     if stat == 0:
